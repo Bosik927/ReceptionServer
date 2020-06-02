b06cf86ef002ab60881e0de5787f461f2219934d
Stanisław Fiuta <Bosik927@gmail.com>
2020-06-02 15:55:52 +0200

    Initial ReceptionApp
    Initial main SpringBootApplication

---
+++ src/main/java/com/github/bosik927/ReceptionApp.java
@@ -0,0 +1,12 @@
+package com.github.bosik927;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionApp.class, args);
+    }
+}
