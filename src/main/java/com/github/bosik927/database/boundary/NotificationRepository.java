5e51d30dbb120d7a57437e32494c4617469c1b59
Stanisław Fiuta <Bosik927@gmail.com>
2020-06-05 14:19:39 +0200

    Refactoring
    Refactoring database repositories

--- src/main/java/com/github/bosik927/database/boundary/NotificationRepository.java
+++ src/main/java/com/github/bosik927/database/boundary/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.github.bosik927.database.boundary;
 
 import com.github.bosik927.database.entity.NotificationEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
